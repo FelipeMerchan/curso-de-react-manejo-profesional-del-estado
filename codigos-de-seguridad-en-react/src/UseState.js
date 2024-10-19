7d09ea4b6208188348252276c71c296717bdf195
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-10-19 14:49:59 -0500

    6. Estados independientes con useState

--- codigos-de-seguridad-en-react/src/UseState.js
+++ codigos-de-seguridad-en-react/src/UseState.js
@@ -1,21 +1,31 @@
 import { useEffect, useState } from "react"
 
+const SECURITY_CODE =  'paradigma'
+
 export const UseState = ({ name }) => {
+  const [value, setValue] = useState('')
   const [error, setError] = useState(false)
   const [loading, setLoading] = useState(false)
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
 
   useEffect(() => {
     console.log('Empezando Effect')
     if (!!loading) {
+      setError(false)
       setTimeout(() => {
         console.log('Cambiar estado loading')
+        if (value !== SECURITY_CODE) {
+          setError(true)
+        }
         setLoading(false)
         console.log('terminar estado loading')
       }, 2000);
     }
     console.log('Terminando Effect')
   }, [loading])
-  
 
   return (
     <>
@@ -28,7 +38,11 @@ export const UseState = ({ name }) => {
           <p>Cargando...</p>
         )}
 
-        <input placeholder="Código de seguridad" />
+        <input
+          placeholder="Código de seguridad"
+          value={value}
+          onChange={handleChange}
+        />
         <button onClick={() => setLoading(true)}>Comprobar</button>
       </>
   )
